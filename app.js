0fa70064b735520ef4734af816da9831df9dfa02
kobe-ryosuke <119792201+kobe-ryosuke@users.noreply.github.com>
2023-01-22 16:54:42 +0900

    Update app.js

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ const clickHandler = (e) => {
     window.alert("正解！");
     score++;
   }else{
-    window.alert("不正解！");
+    window.alert("不正解！ 正解は" + quiz[quizIndex].correct +　"でした！");
   }
 
   quizIndex++;
